9948e24c4821b3cc1ef3f8790ea372478d9dd866
github.com/mennamohamed123-lang <mennamohamed2006m@gmail.com>
2026-07-19 12:51:37 +0300

    javascript

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+var degree = prompt("Enter your degree :");
+
+if (degree >= 90) {
+    alert("A");
+} 
+else if (degree <90 && degree >80) {
+    alert("B");
+}
+else if (degree <80 && degree >70){
+    alert("C")
+}
+else{
+    alert("F")
+}
